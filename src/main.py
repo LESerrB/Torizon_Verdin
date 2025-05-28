d4bed0e6c145627e6076e3b5d71282cbe7e35170
Luis Esteban Serrano <LESerranoB@outlook.com>
2025-05-28 10:45:43 -0600

    Configurado servidor local

--- src/main.py
+++ src/main.py
@@ -1,38 +1,39 @@
 #!python
 
-import time
-import struct
-
+# import time
+# import struct
 from flask import Flask, render_template
+import os
 
-from i2c.sht21 import sht21
-from spi.bme280 import bme280
-from gpio.hx711 import hx711
-from adc.hw504 import hw504
+# from i2c.sht21 import sht21
+# from spi.bme280 import bme280
+# from gpio.hx711 import hx711
+# from adc.hw504 import hw504
 
-app = Flask(__name__)
+# if __name__ == '__main__':
+#     while True:
+#         #-------------------Sensor Humedad/Temperatutra-------------------#
+#         temp, hum = struct.unpack("ff", sht21())
+#         print(f"Temperatura: {temp:.2f} °C")
+#         print(f"Humedad: {hum:.2f} %")
+#         #---------------Sensor Presion/Humedad/Temperatura----------------#
+#         temp280, pres280, hum280 = struct.unpack("fff", bme280())
+#         print(f"Temp: {temp280:.2f} °C | Press: {pres280:.2f} hPa | Hum: {hum280:.2f} %")
+#         #-----------------------------Bascula-----------------------------#
+#         peso711 = hx711()
+#         print(f"Peso: {peso711:.2f} unidades")
+#         #-----------------------------Joystick----------------------------#
+#         x_val, y_val, button_val = struct.unpack("iii", hw504())
+#         print(f"X: {x_val} | Y: {y_val} | Btn: {button_val}")
 
-@app.route('/')
+#         time.sleep(1)
 
-def home():
-    return render_template('home.html')
+template_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "web", 'templates')
+app = Flask(__name__, template_folder=template_dir)
 
-if __name__ == '__main__':
-    app.run()
+@app.route("/")
+def index():
+    return render_template("index.html") #templates/index.html
 
-    while True:
-        #-------------------Sensor Humedad/Temperatutra-------------------#
-        temp, hum = struct.unpack("ff", sht21())
-        print(f"Temperatura: {temp:.2f} °C")
-        print(f"Humedad: {hum:.2f} %")
-        #---------------Sensor Presion/Humedad/Temperatura----------------#
-        temp280, pres280, hum280 = struct.unpack("fff", bme280())
-        print(f"Temp: {temp280:.2f} °C | Press: {pres280:.2f} hPa | Hum: {hum280:.2f} %")
-        #-----------------------------Bascula-----------------------------#
-        peso711 = hx711()
-        print(f"Peso: {peso711:.2f} unidades")
-        #-----------------------------Joystick----------------------------#
-        x_val, y_val, button_val = struct.unpack("iii", hw504())
-        print(f"X: {x_val} | Y: {y_val} | Btn: {button_val}")
-
-        time.sleep(1)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
