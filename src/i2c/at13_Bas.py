37c97fae8b2b28c948e669f36e74294fb17dbf1c
Luis Esteban Serrano <LESerranoB@outlook.com>
2025-09-12 11:39:45 -0600

    Tarjeta de báscula

---
+++ src/i2c/at13_Bas.py
@@ -0,0 +1,30 @@
+import time
+
+from smbus2 import SMBus   # I2C
+
+ADDR_BASC = 0x28
+
+WRITE_REG_ADDR = 0x00
+READ_REG_ADDR = 0x01
+
+def read_Bascula():
+    try:
+        with SMBus(3) as bus:   # 3
+            bus.write_byte_data(ADDR_BASC, WRITE_REG_ADDR, 0x01)
+            bus.write_byte_data(ADDR_BASC, WRITE_REG_ADDR, 0x55)
+
+            time.sleep(0.1)
+
+            read_byte1 = bus.read_byte_data(ADDR_BASC, READ_REG_ADDR)
+            read_byte2 = bus.read_byte_data(ADDR_BASC, READ_REG_ADDR)
+            peso = ((read_byte2 << 8) | read_byte1)/1000
+
+            return peso
+    except Exception as e:
+        print("Error:", e)
+
+def calib_Bascula():
+    print("Calibración")
+
+def tarar_Bascula():
+    print("Proceso de Taraje")
--- src/i2c/sht21.py
+++ src/i2c/sht21.py
@@ -16,13 +16,6 @@ CMD_MEASURE_HUM = 0xF5          # Registro de Humedad
 I2C_ADDR_TMP275 = 0x48          # Dirección TMP275
 TEMP_REGISTER = 0x00            # Registro Temperatura
 
-#  Segunda sonda
-I2C_ADDR_2s_1 = 0x40            # Dirección Tarjeta 2a Sonda
-
-SONDA2_SEND = 0x44  # Esto puede cambiar dinámicamente
-OFFSET_PIEL2 = 0
-I2C_BUS = 1         # Reemplazar con el bus correcto
-ERRORES_SONDA = 0   # Global
 # ===============================================================#
 #               Configuración de offsets y escalas               #
 # ===============================================================#
@@ -109,73 +102,6 @@ def stop_sht21():
     except Exception as e:
         print(f"No se pudo finalizar conexión con SHT21: {e}")
         # logger.warning(f"No se pudo finalizar conexión con SHT21: {e}")
-
-#===============================================================#
-#               Función de Prueba tarjeta 2a Sonda              #
-#===============================================================#
-# Corregir el envío de datos bsandose en la comunicación de la tarjeta de bascula at13 bas
-def readTarjeta2S():
-    global SONDA2_SEND, ERRORES_SONDA
-    ERR_SON_ACUM = 0
-    dato_sonda_1 = 0
-    dato_sonda_2 = 0
-    LECT_SONDA = 0
-
-    time.sleep(0.005)
-
-    try:
-        with SMBus(3) as bus:
-            try:
-                bus.write_byte(I2C_ADDR_2s_1, 0x01)
-                bus.write_byte(I2C_ADDR_2s_1, SONDA2_SEND)
-            except Exception:
-                ERR_SON_ACUM += 1
-                # print("Error",ERR_SON_ACUM)
-
-            time.sleep(0.005)
-
-            try:
-                datos = bus.read_i2c_block_data(I2C_ADDR_2s_1, 0x00, 2)
-                dato_sonda_1 = datos[0]
-                dato_sonda_2 = datos[1]
-                print(dato_sonda_1, dato_sonda_2)
-            except Exception:
-                ERR_SON_ACUM += 1
-                # print("Error",ERR_SON_ACUM)
-
-            LECT_SONDA = (dato_sonda_1 << 8) | dato_sonda_2
-
-            if SONDA2_SEND == 0x44:
-                if LECT_SONDA == 100:
-                    SONDA2_SEND = 0x44
-                elif LECT_SONDA == 150:
-                    SONDA2_SEND = 0x55
-
-            if SONDA2_SEND == 0x33 and LECT_SONDA in [49, 28]:
-                SONDA2_SEND = 0x55
-
-            LECT_SONDA += OFFSET_PIEL2
-
-            # print("\n\n", LECT_SONDA, "\n\n")
-            time.sleep(0.001)
-
-            if ERR_SON_ACUM > 1:
-                ERRORES_SONDA = min(10, ERRORES_SONDA + 1)
-            else:
-                ERRORES_SONDA = 0
-
-            # return ERR_SON_ACUM if auto_pru else LECT_SONDA
-
-
-            # p = read_sensor(bus, 0x55, I2C_ADDR_2s) # Función Normal
-            # print(p)
-            # p = read_sensor(bus, 0x33, I2C_ADDR_2s) # Autoprueba
-            # print(p)
-            # p = read_sensor(bus, 0x44, I2C_ADDR_2s) # Calibración
-            # # si tiene una sonda patron manda primero un 100 y luego un 150 para avisar que ya se calibró
-            # print(p)
-    except Exception as e:
-        print(f"Error de lectura: {e}")
 
 #===============================================================#
 #                    Función de Prueba TMP275                   #
