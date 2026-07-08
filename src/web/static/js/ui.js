45f667116d5791c506dee719e9056464e32df773
Luis Esteban Serrano <leserranob@outlook.com>
2026-07-08 09:49:12 -0600

    Cambio entre paneles

--- src/web/static/js/ui.js
+++ src/web/static/js/ui.js
@@ -8,6 +8,8 @@ const ajstCtrl_Fot = document.getElementById('mod-fot');
 const homeDiv = document.getElementById('home');
 const panelControl = document.getElementById('panel-control');
 
+const btn_cancel = document.getElementById('cancel-ctrl');
+
 function toggleHomePanel(showPanelControl) {
     if (!homeDiv || !panelControl) return;
 
@@ -29,8 +31,6 @@ pnlAire.addEventListener('click', () => {
     console.log("Ajuste de Ctrl de Temperatura de Bebe");
 });
 
-
-
 ajstCtrl_Ox.addEventListener('click', () => {
     console.log("Ajuste de Ctrl de Temperatura de Oxigeno")
 });
@@ -41,4 +41,10 @@ ajstCtrl_Hum.addEventListener('click', () => {
 
 ajstCtrl_Fot.addEventListener('click', () => {
     console.log("Ajuste de Ctrl de Temperatura de Fototerapia")
+});
+
+
+// Boton Cancelar
+btn_cancel.addEventListener('click', () => {
+    toggleHomePanel(false);
 });
