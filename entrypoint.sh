64c48fe28c95b05998143ea892e54ee007ac1928
Luis Esteban Serrano <leserranob@outlook.com>
2026-07-20 16:42:19 -0600

    Limpieza de cache

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Activa el entorno virtual
-source /home/torizon/app/.venv/bin/activate
-# Arranca tu aplicación en segundo plano
-python3 /home/torizon/app/src/main.py &
-# Arranca el SSHD en primer plano
-exec /usr/sbin/sshd -D
--- src/dev/Sensores_TPH/sns_Ox.py
+++
@@ -1,4 +0,0 @@
-from api.pins_ADC import read_adc
-
-def read_SnsOx(adc_chn: int):
-    return read_adc(adc_chn)
