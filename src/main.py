ac810100dc362eef6e15b94a43a9adcbcc8ff9b1
Luis Esteban Serrano <leserranob@outlook.com>
2026-07-31 11:57:15 -0600

    Correción de bugs de Merge

--- src/main.py
+++ src/main.py
@@ -15,8 +15,8 @@ from flask_cors import CORS
 # load_dotenv("/mnt/microsd/.env")
 # logger.info('Encendido del sistema')
 
-# from dev.Controles_Alertas import encoder as hw_encoder
-# from dev.Comunicacion import bascula as com_bascula
+from dev.Controles_Alertas import encoder as hw_encoder
+from dev.Comunicacion import bascula as com_bascula
 
 # from api.files.tendencias import agregarDtTemperatura, limpiarDtTemperatura
 #------------------------- En Pruebas -------------------------#
@@ -125,7 +125,7 @@ def enEditCtrls():
     val_Encd = valores_ctrl[edit_Ctrl]
     valores_ctrl["confirm"] = ctrl.get("Enable")
 
-    # hw_encoder.valConfig(edit_Ctrl)
+    hw_encoder.valConfig(edit_Ctrl)
 
     return jsonify(
         {
@@ -156,7 +156,7 @@ def ctrlEncd():
             }
         ), 200
     except:
-        monitor_pause.set()                 # Reinicio de Monitoreo
+        monitor_pause.set()                     # Reinicio de Monitoreo
 
         return jsonify(
             {
@@ -164,24 +164,24 @@ def ctrlEncd():
             }
         ), 400
 
-# @app.route("/api/pesar", methods=["POST"])
-# def api_Pesaje():
-#     global pesoTCD
+@app.route("/api/pesar", methods=["POST"])
+def api_Pesaje():
+    global pesoTCD
 
-    # peso = round(com_bascula.pesaje(), 3)
+    peso = round(com_bascula.pesaje(), 3)
 
-    # print(f"=======Fin Pesaje: {peso}=======")
+    print(f"=======Fin Pesaje: {peso}=======")
 
-    # if peso != 999:
-    #     pesoTCD = int(peso * 1000)
+    if peso != 999:
+        pesoTCD = int(peso * 1000)
 
-    #     if peso > 10:
-    #         pesoTCD = int((peso - 7) * 1000)
-    #         peso = round((pesoTCD/1000), 3)
+        if peso > 10:
+            pesoTCD = int((peso - 7) * 1000)
+            peso = round((pesoTCD/1000), 3)
 
-    #     return jsonify({"status": "ok", "peso": peso}), 200
-    # else:
-    # return jsonify({"status": "fail"}), 400
+        return jsonify({"status": "ok", "peso": peso}), 200
+    else:
+        return jsonify({"status": "fail"}), 400
 
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>#
 #                            Funciones de sistema                            #
@@ -191,7 +191,7 @@ def sys_monitor():
         monitor_pause.wait()
         restart_container()         # Memoria del contenedor
 
-        # TCD(vls_snsrsTCD)           # Envío de datos a la TCD
+        TCD(vls_snsrsTCD)           # Envío de datos a la TCD
 
         time.sleep(0.1)
 
@@ -206,18 +206,18 @@ def restart_container(threshold=90):
 
 def encoder_Reader():
     global edit_Ctrl, val_Encd
-    # hw_encoder.init_encoder()
+    hw_encoder.init_encoder()
 
-    # while True:
-    #     if valores_ctrl["confirm"]:
-    #         nuevo_val = hw_encoder.valEdit(val_Encd)
+    while True:
+        if valores_ctrl["confirm"]:
+            nuevo_val = hw_encoder.valEdit(val_Encd)
 
-    #         if nuevo_val != val_Encd:
-    #             val_Encd = nuevo_val
+            if nuevo_val != val_Encd:
+                val_Encd = nuevo_val
 
-    #         valores_ctrl["confirm"] = hw_encoder.swAcept()
+            valores_ctrl["confirm"] = hw_encoder.swAcept()
 
-    #     time.sleep(0.005)
+        time.sleep(0.005)
 
 #============================================================================#
 #                                    Hilos                                   #
